dfbd2e773033e25ebe1981c499faad40539837cb
filipa <filipadurao@gmail.com>
2018-03-02 16:56:39 +0000

    Ex1 of exercise sheet 3

---
+++ Ficha_3/ex_1.c
@@ -0,0 +1,26 @@
+// PROGRAMA p1.c
+#include <stdio.h>
+#include <unistd.h>
+
+int global=1;
+
+int main(void) {
+    
+    int local = 2;
+    
+    if(fork() > 0) {    // Processo Pai
+        printf("PID = %d; PPID = %d\n", getpid(), getppid());
+        global++;
+        local--;
+    } 
+    
+    else {  // Se filho ou erro
+        printf("PID = %d; PPID = %d\n", getpid(), getppid());
+        global--;
+        local++;
+    }
+    
+    printf("PID = %d - global = %d ; local = %d\n", getpid(), global, local);
+ 
+    return 0;
+} 
